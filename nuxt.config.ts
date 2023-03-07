aa07e225a56283d5034d93270fac373750d33f7e
Thiagonox <thiagogentil499@gmail.com>
2023-03-07 09:03:21 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+export default {
+    build: {
+      // ...
+    },
+    server: {
+      // Configurações do servidor de desenvolvimento
+      // Exemplo:
+      port: 3000, // porta usada pelo servidor de desenvolvimento
+      host: 'localhost', // endereço usado pelo servidor de desenvolvimento
+    },
+    // Configurações do Vite
+    vite: {
+      // Configurações do Vite
+    }
+  }
+  
